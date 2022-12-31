7510cea71b8fb9b61e3f2a8c75f20ee1c24baf54
chidiebere <chidiebereuzomahumble@gmail.com>
2022-12-31 11:58:53 +0100

    increased nearby stores count to 100

--- routes/places.js
+++ routes/places.js
@@ -6,7 +6,7 @@ const apiKey = 'NGndxGqMBy3xc3bCQhpHq7H5sVfwymGX'
 
 router.get('/:lat/:lon', async (req, res) => {
   try {
-    const response = await superagent.get(`https://api.tomtom.com/search/2/nearbySearch/.json?limit=12&key=${apiKey}&lat=${req.params.lat}&lon=${req.params.lon}`)
+    const response = await superagent.get(`https://api.tomtom.com/search/2/nearbySearch/.json?limit=100&key=${apiKey}&lat=${req.params.lat}&lon=${req.params.lon}`)
     if (response.statusCode === 200) {
       return res.json(response.body.results);
     }
